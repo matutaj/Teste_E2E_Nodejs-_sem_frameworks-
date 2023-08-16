798eba4921d19150ffa14b92d19e18691c4a23b6
matutaj <matutajorge2002@gmail.com>
2023-08-16 17:25:21 +0100

    feat: add it and strictequal, user and password

--- api.js
+++ api.js
@@ -1,6 +1,22 @@
+import { once } from "node:events"
 import { createServer } from "node:http"
 
+const VALID = {
+    user: "matutajorge",
+    password: "1234"
+}
+
+async function loginRoute(request, response) {
+    const { user, password } = JSON.parse(await once(request, 'data'))
+    console.log({ user, password })
+    response.end("ok")
+}
+
 async function handle(request, response) {
+
+    if (request.url === '/login' && request.method === 'POST') {
+        return loginRoute(request, response)
+    }
     response.end("Hello World!");
 }
 
--- api.test.js
+++ api.test.js
@@ -1,14 +1,25 @@
 import { describe, before, after, it } from "node:test";
-
+import { deepStrictEqual, strictEqual } from "node:assert";
+const BASE_URL = 'http://localhost:1090'
 describe("API WorkFlow", () => {
     let _server = {}
 
     before(async () => {
-        _server = (await import("./api")).app
+        _server = (await import("./api.js")).app
         await new Promise(resolve => _server.once("listening", resolve))
     })
     after(done => _server.close(done))
     it('Should receive not autorized given wrong user and password', async () => {
-
+        const data = {
+            user: "matutajorge",
+            password: ""
+        }
+        const request = await fetch(`${BASE_URL}/login`, {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        strictEqual(request.status, 401)
+        const response = await request.json()
+        deepStrictEqual(response, { error: 'User invalid!' })
     })
 })
